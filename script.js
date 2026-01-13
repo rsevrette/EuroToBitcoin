80ddf83a30ad6175a259bdb926942c9eec41eb3f
ant1Da <antoine.dast@edouardgand.fr>
2026-01-13 10:47:16 +0100

    CorrectionBugFormule

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function convertir() {
-    let euro = document.getElementById("euro").ariaValueMax;
+    let euro = parseFloat(document.getElementById("euro").value);
 
     // Taux au 10/01/2026 : 1BTC = 77 600€
     let taux = 77600;
 
     //BUG: la formule est incorrecte
-    let bitcoin = euro * taux;
+    let bitcoin = euro / taux;
 
     document.getElementById("resultat").textContent = 
         euro + " € = " + bitcoin + " BTC";
